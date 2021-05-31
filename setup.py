efefc08d994f64cb3dba56344f2bc3d5b2bafac5
Jianming-Han <jianminghan@126.com>
2021-05-31 15:22:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,20 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='nqviz',
-      version='0.0.5',
+      version='0.0.6',
       description='N Queen Visualizer',
-      packages=['nqviz'],
-      install_requires=['pygame>=2.0.1'],
-      long_description=long_description,
-      long_description_content_type='text/markdown',
+      url='https://github.com/jhan15/nqviz',
       author = 'Jianming Han',
       author_email = 'hjma810@126.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.8',
+      ],
+      packages=['nqviz'],
+      install_requires=['pygame>=2.0.1'],
+      python_requires='>=3',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       zip_safe=False)
