f3be4f0e5b6b856b90a9a4f88acd6645f8ff2e6c
Jianming-Han <jianminghan@126.com>
2021-05-31 13:28:04 +0200

    Bug fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nqviz',
-      version='0.0.1',
+      version='0.0.2',
       description='N Queen Visualizer',
       packages=['nqviz'],
       install_requires=['pygame>=2.0.1'],
