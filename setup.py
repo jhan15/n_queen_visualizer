73afd2bf3d2b2a66a702af906240648346b6b8a4
Jianming-Han <jianminghan@126.com>
2021-05-31 09:03:35 +0200

    Build package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nqviz',
+      version='0.0.1',
+      description='N Queen Visualizer',
+      packages=['nqviz'],
+      author = 'Jianming Han',
+      author_email = 'hjma810@126.com',
+      zip_safe=False)
